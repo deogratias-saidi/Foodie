3fe7b3ab7de3bca4ddb8f21f8dfb94fe2343bb7c
Deogratias Djuma Saidi <50410476+deogratias-saidi@users.noreply.github.com>
2021-10-29 00:07:02 +0200

    add splash screens

--- Foodie-Front-End/src/App.vue
+++ Foodie-Front-End/src/App.vue
@@ -1,22 +1,3 @@
 <template>
-  <div>
-
-  </div>
+  <Router-View/>
 </template>
-
-<script>
-import { reactive, toRefs } from 'vue'
-
-export default {
-  setup () {
-    const state = reactive({
-      count: 0,
-    })
-  
-    return {
-      ...toRefs(state),
-    }
-  }
-}
-</script>
-
---
+++ Foodie-Front-End/src/pages/Splash/SplashSign.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="bg-[#F2FEFF] w-screen h-screen">
+    <div>
+      <img src="../../assets/SVG/couplesInTheKitchen.svg" />
+    </div>
+    <div class="">
+      <div
+        class="
+          font1
+          text-[#12A84F]
+          flex
+          content-center
+          text-center text-[52px]
+          leading-[57.5px]
+        "
+      >
+        IT'S COOKING TIME
+      </div>
+      <div class="flex justify-center pt-[56px]">
+        <div class="font2 text-[14px] mr-3">Don't have an Account</div>
+        <div class="font2 text-[14px] font-semibold text-blue-700">Sign Up</div>
+      </div>
+      <div class="font2 mt-8 text-white h-[60px] w-full flex items-center justify-center bg-[#1EBC5D] text-[18px]">
+          Let’s Start Cooking 
+          <box-icon name='right-arrow-alt' color='#ffffff' ></box-icon>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Rozha+One&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400&display=swap");
+
+.font1 {
+  font-family: "Rozha One", serif;
+}
+
+.font2 {
+  font-family: "Montserrat", serif;
+}
+</style>
