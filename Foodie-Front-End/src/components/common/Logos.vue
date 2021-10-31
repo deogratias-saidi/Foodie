cd728530165b235c4c0b12f271c7f5778bda8142
Deogratias Djuma Saidi <50410476+deogratias-saidi@users.noreply.github.com>
2021-10-31 07:30:53 +0100

    Modification on the sign in page and add sign up page

---
+++ Foodie-Front-End/src/components/common/Logos.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex justify-between items-center">
+        <img src="../../assets/SVG/microsoft.svg" alt="Microsofft">
+        <img src="../../assets/SVG/facebook.svg" alt="Facebook">
+        <img src="../../assets/SVG/apple.svg" alt="Apple">
+        <img src="../../assets/SVG/google.svg" alt="Google">
+    </div>
+</template>
