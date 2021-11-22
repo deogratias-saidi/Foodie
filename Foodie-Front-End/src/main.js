e3991fc87b780938b7f4a81d4942a37308bb174c
deogratias-saidi <deogratias.saidi@outlook.com>
2021-11-22 19:56:32 +0100

    remove fontaswesome from local

--- Foodie-Front-End/src/main.js
+++ Foodie-Front-End/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import './index.css'
-import './assets/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 createApp(App).use(router).mount('#app')
